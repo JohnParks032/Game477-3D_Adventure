ce1cb53d78359f2f70211c71b50f63c9643fe1e2
JohnParks032 <80427077+JohnParks032@users.noreply.github.com>
2023-05-01 17:52:26 -0500

    Added Scripts folder, Added AudioManager & Game Function for AudioManager capabilities

---
+++ 3D_Adventure/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SoundType
+{
+    BTN,
+    BURGER_BUILD,
+    BURGER_COOK,
+    FRIES_COOK,
+    IC_BREAK,
+    ITEM_COMPLETE,
+    MAIN_BG,
+    MENU_BG,
+    ORDER_COMPLETE
+}
+
+public struct Range
+{
+    public float min;
+    public float max;
+
+    public Range(float min, float max)
+    {
+        this.min = min;
+        this.max = max;
+    }
+
+    public float RandomValue()
+    {
+        return UnityEngine.Random.Range(min, max);
+    }
+}
+
+public class SoundCollection
+{
+    private AudioClip[] clips;
+
+    public bool HasClips
+    {
+        get { return clips.Length > 0; }
+    }
+
+    public SoundCollection(params string[] soundNames)
+    {
+        clips = new AudioClip[soundNames.Length];
+        for (int i = 0; i < soundNames.Length; i++)
+        {
+            clips[i] = Resources.Load<AudioClip>(soundNames[i]);
+            if (clips[i] == null)
+            {
+                MonoBehaviour.print($"Can't find clip with name '{soundNames[i]}'");
+            }
+        }
+    }
+
+    public AudioClip RandomClip()
+    {
+        if (clips.Length == 0)
+        {
+            return null;
+        }
+        else
+        {
+            int index = UnityEngine.Random.Range(0, clips.Length);
+            return clips[index];
+        }
+    }
+}
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+    public float masterVolumeMult = 1.0f;
+    public static readonly Range pitchRange = new Range(0.75f, 1.25f);
+    public static readonly Range volRange = new Range(0.75f, 1.0f);
+
+    private AudioSource audioSrc;
+    private Dictionary<SoundType, SoundCollection> sounds;
+
+    private void Start()
+    {
+        audioSrc = GetComponent<AudioSource>();
+        sounds = new Dictionary<SoundType, SoundCollection>() {
+      {SoundType.BTN, new SoundCollection("btn") },
+      {SoundType.BURGER_BUILD, new SoundCollection("burger_build") },
+      {SoundType.BURGER_COOK, new SoundCollection("burger_cook") },
+      {SoundType.FRIES_COOK, new SoundCollection("fries_cook") },
+      {SoundType.IC_BREAK, new SoundCollection("ic_break") },
+      {SoundType.ITEM_COMPLETE, new SoundCollection("item_complete") },
+      {SoundType.MAIN_BG, new SoundCollection("main_bg") },
+      {SoundType.MENU_BG, new SoundCollection("menu_bg") },
+      {SoundType.ORDER_COMPLETE, new SoundCollection("order_complete") }
+    };
+    }
+
+    public void PlaySound(SoundType type, bool allowPitchShift = true, bool allowVolShift = true)
+    {
+        PlaySound(type, audioSrc, allowPitchShift, allowVolShift);
+    }
+
+    public void PlayOnce(SoundType type, bool allowPitchShift = true, bool allowVolShift = true)
+    {
+        PlayOnce(type, audioSrc);
+    }
+    public void PlayMusic(SoundType type, bool allowPitchShift = true, bool allowVolShift = true)
+    {
+        PlayMusic(type, audioSrc);
+    }
+
+    private void PlayOnce(SoundType type, AudioSource audioSrc)
+    {
+        if (audioSrc == null)
+        {
+            audioSrc = this.audioSrc;
+        }
+        if (sounds.ContainsKey(type) && sounds[type].HasClips)
+        {
+            if (audioSrc.gameObject.activeSelf)
+            {
+                audioSrc.clip = sounds[type].RandomClip();
+                audioSrc.PlayOneShot(audioSrc.clip, 0.01f);
+            }
+        }
+    }
+
+    private void PlaySound(SoundType type, AudioSource audioSrc, bool allowPitchShift = true, bool allowVolShift = true)
+    {
+        if (audioSrc == null)
+        {
+            audioSrc = this.audioSrc;
+        }
+        if (sounds.ContainsKey(type) && sounds[type].HasClips)
+        {
+            if (audioSrc.gameObject.activeSelf)
+            {
+                audioSrc.pitch = allowPitchShift ? pitchRange.RandomValue() : 1.0f;
+                audioSrc.volume = allowVolShift ? volRange.RandomValue() : 1.0f;
+                audioSrc.volume *= masterVolumeMult;
+                audioSrc.clip = sounds[type].RandomClip();
+                audioSrc.Play();
+            }
+        }
+    }
+
+    private void PlayMusic(SoundType type, AudioSource audioSrc, bool allowPitchShift = true, bool allowVolShift = true)
+    {
+        if (audioSrc == null)
+        {
+            audioSrc = this.audioSrc;
+        }
+        if (sounds.ContainsKey(type) && sounds[type].HasClips)
+        {
+            if (audioSrc.gameObject.activeSelf)
+            {
+                audioSrc.volume *= masterVolumeMult;
+                audioSrc.clip = sounds[type].RandomClip();
+                audioSrc.Play();
+            }
+        }
+    }
+}
