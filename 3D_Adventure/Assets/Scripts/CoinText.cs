ff8c777f8909aebb3354219601591f046da43aaa
JohnParks032 <80427077+JohnParks032@users.noreply.github.com>
2023-05-10 23:41:23 -0500

    added coin counter UI

---
+++ 3D_Adventure/Assets/Scripts/CoinText.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CoinText : MonoBehaviour
+{
+    public TextMeshProUGUI coinText;
+    int coinCount;
+    public void OnEnable()
+    {
+        Coin.OnCoinCollected += IncrementCoinCount;
+    }
+    public void OnDisable()
+    {
+        Coin.OnCoinCollected -= IncrementCoinCount;
+    }
+
+    public void IncrementCoinCount()
+    {
+        coinCount++;
+        coinText.text = $"Acorns: {coinCount} / 40";
+    }
+}
