361e9947df8d65e953ce2e567a68ee5acf29232c
JohnParks032 <80427077+JohnParks032@users.noreply.github.com>
2023-05-18 13:56:42 -0500

    changed how controls menu works

--- 3D_Adventure/Assets/Scripts/SceneManagement.cs
+++ 3D_Adventure/Assets/Scripts/SceneManagement.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagement : MonoBehaviour
 {
+    public GameObject ControlsMenuCanvas;
+    public GameObject MainMenuCanvas;
+
     public async void StartScene()
     {
         Cursor.visible = false;
@@ -25,12 +28,24 @@ public class SceneManagement : MonoBehaviour
         await Task.Delay(1000);
         SceneManager.LoadScene("Game");
     }
+
+    public async void HomeButton()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        await Task.Delay(1000);
+        MainMenuCanvas.SetActive(true);
+        ControlsMenuCanvas.SetActive(false);
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
     public async void Options()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
         await Task.Delay(1000);
-        SceneManager.LoadScene("Controls Scene");
+        ControlsMenuCanvas.SetActive(true);
+        MainMenuCanvas.SetActive(false);
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
     }
